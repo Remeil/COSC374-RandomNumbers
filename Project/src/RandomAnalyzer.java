38d72fdea15b5af7f30bab238fddfca4948c8944
Remeil <stilesjohnathan21@gmail.com>
2016-03-09 13:43:55 -0500

    Add method to determine gcd of two numbers.

--- Project/src/RandomAnalyzer.java
+++ Project/src/RandomAnalyzer.java
@@ -6,12 +6,21 @@
  */
 public class RandomAnalyzer {
     public static void main(String[] args) {
-	System.out.println(countRandomGcds(5000, 1, RandomMode.MathRandom));
-	System.out.println(countRandomGcds(5000, 500, RandomMode.RandomObject));
+	System.out.println(gcd(3600, 1800));
     }
     
     private static int countRandomGcds(int numberOfPairsToGenerate, int seed, RandomMode mode) {
 	return 15;
+    }
+    
+    private static int gcd(int num1, int num2) {
+	//Extended Euclidian Algorithm
+	if (num2 == 0) {
+	    return num1;
+	}
+	else {
+	    return gcd(num2, num1 % num2);
+	}
     }
     
     private enum RandomMode {
