e2328c3bab71b1d6b2faf76397ea903b2d61ffd2
Remeil <stilesjohnathan21@gmail.com>
2016-03-09 13:35:58 -0500

    Add seed as a parameter.

--- Project/src/RandomAnalyzer.java
+++ Project/src/RandomAnalyzer.java
@@ -6,11 +6,11 @@
  */
 public class RandomAnalyzer {
     public static void main(String[] args) {
-	System.out.println(countRandomGcds(5000, RandomMode.MathRandom));
-	System.out.println(countRandomGcds(5000, RandomMode.RandomObject));
+	System.out.println(countRandomGcds(5000, 1, RandomMode.MathRandom));
+	System.out.println(countRandomGcds(5000, 500, RandomMode.RandomObject));
     }
     
-    private static int countRandomGcds(int numberOfPairsToGenerate, RandomMode mode) {
+    private static int countRandomGcds(int numberOfPairsToGenerate, int seed, RandomMode mode) {
 	return 15;
     }
     
