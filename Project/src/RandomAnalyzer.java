82f22fde7b554a2e1a9fb03d896402b45738a96d
Remeil <stilesjohnathan21@gmail.com>
2016-03-09 13:28:54 -0500

    Stub out methods.

--- Project/src/RandomAnalyzer.java
+++ Project/src/RandomAnalyzer.java
@@ -6,6 +6,16 @@
  */
 public class RandomAnalyzer {
     public static void main(String[] args) {
-	
+	System.out.println(countRandomGcds(5000, RandomMode.MathRandom));
+	System.out.println(countRandomGcds(5000, RandomMode.RandomObject));
+    }
+    
+    private static int countRandomGcds(int numberOfPairsToGenerate, RandomMode mode) {
+	return 15;
+    }
+    
+    private enum RandomMode {
+	MathRandom,
+	RandomObject
     }
 }
